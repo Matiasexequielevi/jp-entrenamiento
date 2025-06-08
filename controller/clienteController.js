0cd7ccbffe08859e268b325aa73df78930f7ea67
Matías Levi <mati_10mono@hotmail.com>
2025-06-08 18:29:58 -0300

    Fix: Express EJS layouts configurado correctamente

--- controller/clienteController.js
+++ controller/clienteController.js
@@ -49,17 +49,6 @@ exports.listarClientes = async (req, res) => {
             numero = '549' + numero;
           }
 
-          const esperarWhatsapp = () => new Promise(resolve => {
-            const check = setInterval(() => {
-              if (whatsappClient.clientReady) {
-                clearInterval(check);
-                resolve();
-              }
-            }, 200);
-          });
-
-          await esperarWhatsapp();
-
           await whatsappClient.sendMessage(numero, mensaje);
           console.log(`📤 Mensaje enviado a ${cliente.nombre}`);
           cliente.notificado = true;
@@ -88,24 +77,7 @@ exports.formularioNuevo = (req, res) => {
 
 exports.guardarCliente = async (req, res) => {
   try {
-    const datos = req.body;
-
-    const nuevoCliente = new Cliente({
-      ...datos,
-      edad: parseInt(datos.edad),
-      fechaInicio: new Date(datos.fechaInicio),
-      fechaPago: new Date(datos.fechaPago),
-      asistencia: {
-        semanal: {
-          lunes: datos['asistencia.semanal.lunes'] === 'true',
-          martes: datos['asistencia.semanal.martes'] === 'true',
-          miercoles: datos['asistencia.semanal.miercoles'] === 'true',
-          jueves: datos['asistencia.semanal.jueves'] === 'true',
-          viernes: datos['asistencia.semanal.viernes'] === 'true'
-        }
-      }
-    });
-
+    const nuevoCliente = new Cliente(req.body);
     await nuevoCliente.save();
     res.redirect('/');
   } catch (error) {
