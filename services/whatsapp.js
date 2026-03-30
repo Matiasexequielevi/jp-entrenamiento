0c715c83766e8220ed0669c6ec75bc8672081b6a
Matías Levi <mati_10mono@hotmail.com>
2026-03-30 18:44:26 -0300

    Corrijo WhatsApp para Render

--- services/whatsapp.js
+++ services/whatsapp.js
@@ -103,7 +103,6 @@ function iniciarWhatsApp() {
     }),
    puppeteer: {
   headless: true,
-  executablePath: '/usr/bin/google-chrome',
   args: [
     '--no-sandbox',
     '--disable-setuid-sandbox',
